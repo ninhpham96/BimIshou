a19b9e5fc93832a59cc33a56463b205954997578
phamninh <phamninh708@gmail.com>
2023-08-25 17:05:26 +0700

    update dim door and window

--- Commands/Command.cs
+++ Commands/Command.cs
@@ -51,6 +51,52 @@ namespace BimIshou.Commands
             AddedElement.AddRange(e.GetAddedElementIds());            
         }
     }
+
+    [UsedImplicitly]
+    [Transaction(TransactionMode.Manual)]
+    public class PostCommanCreateCeiling : ExternalCommand
+    {
+        //public static bool ReadyToAdd;
+        public static List<ElementId> AddedElement;
+        public static UIDocument UIDoc;
+        public static UIApplication uiapp;
+        public static Autodesk.Revit.ApplicationServices.Application app;
+        public static event EventHandler OnPostableCommandModelLineEnded;
+        public override void Execute()
+        {
+            //ReadyToAdd = true;
+            AddedElement = new List<ElementId>();
+            OnPostableCommandModelLineEnded = null;
+            RevitCommandEndedMonitor revitCommandEndedMonitor = new RevitCommandEndedMonitor(uiapp);
+            revitCommandEndedMonitor.CommandEnded += RevitCommandEndedMonitor_CommandEnded;
+            app.DocumentChanged += Application_DocumentChanged;
+
+            RevitCommandId cmdModelLine_id = RevitCommandId.LookupPostableCommandId(PostableCommand.AutomaticCeiling);
+            uiapp.PostCommand(cmdModelLine_id);
+        }
+        /// <summary>
+        /// Register OnPostableCommandModelLineEnded after start
+        /// Start on any Cmd
+        /// Finist => List<DetailLine> AddedDetailLines
+        /// </summary>
+        /// <param name="uidoc">UIDocument</param>
+        public static void Start(UIApplication uiap)
+        {
+            uiapp = uiap;
+            UIDoc = uiapp.ActiveUIDocument;
+            app = uiapp.Application;
+            new PostCommanCreateCeiling().Execute();
+        }
+        private void RevitCommandEndedMonitor_CommandEnded(object sender, EventArgs e)
+        {
+            app.DocumentChanged -= Application_DocumentChanged;
+            OnPostableCommandModelLineEnded?.Invoke(this, EventArgs.Empty);
+        }
+        private void Application_DocumentChanged(object sender, Autodesk.Revit.DB.Events.DocumentChangedEventArgs e)
+        {
+            AddedElement.AddRange(e.GetAddedElementIds());
+        }
+    }
     /// <summary>
     /// https://forums.autodesk.com/t5/revit-api-forum/how-do-i-continue-after-postcommand/td-p/10981164
     /// https://gist.github.com/ricaun/cc4f0a39b36006883f091bc7f0fc3d35
--- Commands/DimA18.cs
+++ Commands/DimA18.cs
@@ -1,7 +1,8 @@
-﻿using Nice3point.Revit.Toolkit.External;
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
 
 namespace BimIshou.Commands;
-
+[Transaction(TransactionMode.Manual)]
 internal class DimA18 : ExternalCommand
 {
     public override void Execute()
