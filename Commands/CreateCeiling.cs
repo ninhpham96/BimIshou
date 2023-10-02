aea56da1341b09b72d36e340601dd2ff3134206d
My PC <My PC@DESKTOP-DG3329I>
2023-10-02 22:39:30 +0700

    create ceilling

--- Commands/CreateCeiling.cs
+++ Commands/CreateCeiling.cs
@@ -1,79 +1,99 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
-using Autodesk.Revit.DB.Events;
-using BimIshou.Views;
+using Autodesk.Revit.UI;
+using BimIshou.Utils;
 using Nice3point.Revit.Toolkit.External;
-using System.Windows;
 
 namespace BimIshou.Commands
 {
     [Transaction(TransactionMode.Manual)]
-    public class CreateCeiling : ExternalCommand
+    public class CreateCeilling : ExternalCommand
     {
+        RevitCommandEndedMonitor revitCommandEndedMonitor;
+        IList<Reference> selectRooms;
+        public List<ElementId> AddedElement { get; set; } = new List<ElementId>();
+        List<ElementId> Ceillings = new();
         public override void Execute()
         {
-            Ceiling ceiling = null;
-            PostCommanCreateCeiling.Start(UiApplication);
-            PostCommanCreateCeiling.OnPostableCommandModelLineEnded += PostCommanDetailLine_OnPostableCommandModelLineEnded;
-
-
-            //_editPlanRegion(new ElementId(2551), lines);
-
-        }
-        private void PostCommanDetailLine_OnPostableCommandModelLineEnded(object sender, EventArgs e)
-        {
-            Ceiling ceiling = null;
-            foreach (ElementId id in PostCommanCreateCeiling.AddedElement)
+            try
             {
-                Element ele = Document.GetElement(id);
+                selectRooms = UiDocument.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element, new SelectionFilter(BuiltInCategory.OST_Rooms, true));
+                revitCommandEndedMonitor = new RevitCommandEndedMonitor(UiApplication);
+                revitCommandEndedMonitor.CommandEnded += OnCommandEnded;
+                Application.DocumentChanged += Application_DocumentChanged;
+                UiApplication.PostCommand(RevitCommandId.LookupPostableCommandId(PostableCommand.AutomaticCeiling));
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return;
+            }
+        }
+        private void Application_DocumentChanged(object sender, Autodesk.Revit.DB.Events.DocumentChangedEventArgs e)
+        {
+            AddedElement.AddRange(e.GetAddedElementIds());
+        }
+        private void OnCommandEnded(object sender, EventArgs e)
+        {
+            Application.DocumentChanged -= Application_DocumentChanged;
+            revitCommandEndedMonitor.CommandEnded -= OnCommandEnded;
+            foreach (var item in AddedElement)
+            {
+                var ele = Document.GetElement(item);
                 if (ele is Ceiling)
                 {
-                    ceiling = (Ceiling)ele;
+                    Ceillings.Add(ele.Id);
                     break;
                 }
             }
-            using (Transaction transs = new Transaction(Document, "test"))
+            using (TransactionGroup tranG = new TransactionGroup(Document, "Auto Create Ceilling"))
             {
-                transs.Start();
-
-
-                ceiling.Copy(XYZ.BasisY * 100);
-
-
-                transs.Commit();
+                tranG.Start();
+                using (Transaction transs = new Transaction(Document, "test"))
+                {
+                    transs.Start();
+                    var opt = new SpatialElementBoundaryOptions();
+                    for (int i = 1; i < selectRooms.Count; i++)
+                    {
+                        var tempCeilling = (Document.GetElement(Ceillings.FirstOrDefault())).Copy(XYZ.BasisY * 100 * i).FirstOrDefault();
+                        Ceillings.Add(tempCeilling);
+                    }
+                    transs.Commit();
+                }
+                for (int i = 0; i < selectRooms.Count; i++)
+                {
+                    ICollection<ElementId> ids;
+                    List<Line> polygon = new List<Line>();
+                    Room room = Document.GetElement(selectRooms[i]) as Room;
+                    IList<IList<BoundarySegment>> loops = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
+                    foreach (IList<BoundarySegment> loop in loops)
+                    {
+                        foreach (BoundarySegment seg in loop)
+                        {
+                            Line line = seg.GetCurve() as Line;
+                            polygon.Add(line);
+                        }
+                    }
+                    using (Transaction transs = new Transaction(Document, "temp"))
+                    {
+                        transs.Start();
+                        ids = Document.Delete(Ceillings[i]);
+                        transs.RollBack();
+                    }
+                    using (Transaction trans = new Transaction(Document, "Change Sketch Ceilling"))
+                    {
+                        trans.Start();
+                        EditCeilling(ids, polygon);
+                        trans.Commit();
+                    }
+                }
+                tranG.Assimilate();
             }
-            //XYZ p1 = new XYZ(0, 0, 0);
-            //XYZ p2 = new XYZ(1000, 0, 0);
-            //XYZ p3 = new XYZ(1000, 1000, 0);
-            //XYZ p4 = new XYZ(0, 1000, 0);
-
-            //Line line1 = Line.CreateBound(p1, p2);
-            //Line line2 = Line.CreateBound(p2, p3);
-            //Line line3 = Line.CreateBound(p3, p4);
-            //Line line4 = Line.CreateBound(p4, p1);
-
-            //List<Line> lines = new List<Line>() { line1, line2, line3, line4 };
-
-            //var xxx = PostCommanCreateCeiling.AddedElement;
-            //_editPlanRegion(xxx, lines);
-            PostCommanCreateCeiling.OnPostableCommandModelLineEnded -= PostCommanDetailLine_OnPostableCommandModelLineEnded;
         }
-
-        private void Application_DocumentChanged(object sender, DocumentChangedEventArgs e)
+        private void EditCeilling(ICollection<ElementId> elementIds, List<Line> polygon)
         {
-            e.GetDeletedElementIds();
-        }
-        private void _editPlanRegion(ElementId planRegion, List<Line> polygon)
-        {
-            using Transaction transTemp = new Transaction(Document, "temp");
-            transTemp.Start();
-            ICollection<ElementId> ids = Document.Delete(planRegion);
-            transTemp.RollBack();
-
-            //Get detailines of plan region
             List<ModelLine> detailLines = new List<ModelLine>();
-            foreach (ElementId id in ids)
+            foreach (ElementId id in elementIds)
             {
                 Element ele = Document.GetElement(id);
                 if (ele is ModelLine)
@@ -81,17 +101,7 @@ namespace BimIshou.Commands
                     detailLines.Add(ele as ModelLine);
                 }
             }
-
-            using Transaction trans = new Transaction(Document);
-            trans.Start("Change Plane Region");
-
-            //Get 1 detailine
             var firstDetailLine = detailLines.FirstOrDefault();
-
-            //Get z
-            var z = firstDetailLine.GeometryCurve.GetEndPoint(0).Z;
-
-            //Create new detail line
             foreach (var line in polygon)
             {
                 Transform transform = Transform.CreateTranslation(new XYZ(0, 0, 1));
@@ -101,52 +111,7 @@ namespace BimIshou.Commands
                 var locCurve = newLine.Location as LocationCurve;
                 locCurve.Curve = Line.CreateBound(transform.OfPoint(line.GetEndPoint(0)), transform.OfPoint(line.GetEndPoint(1)));
             }
-
-            //Delete old detail lines
             Document.Delete(detailLines.Select(x => x.Id).ToList());
-
-            trans.Commit();
         }
-        private void _editPlanRegion(List<ElementId> elementIds, List<Line> polygon)
-        {
-            ICollection<ElementId> ids = elementIds;
-
-            //Get detailines of plan region
-            List<ModelLine> detailLines = new List<ModelLine>();
-            foreach (ElementId id in ids)
-            {
-                Element ele = Document.GetElement(id);
-                if (ele is ModelLine)
-                {
-                    detailLines.Add(ele as ModelLine);
-                }
-            }
-
-            using Transaction trans = new Transaction(Document);
-            trans.Start("Change Plane Region");
-
-            //Get 1 detailine
-            var firstDetailLine = detailLines.FirstOrDefault();
-
-            //Get z
-            var z = firstDetailLine.GeometryCurve.GetEndPoint(0).Z;
-
-            //Create new detail line
-            foreach (var line in polygon)
-            {
-                Transform transform = Transform.CreateTranslation(new XYZ(0, 0, 1));
-                var tf = Transform.Identity;
-                var newID = ElementTransformUtils.CopyElement(Document, firstDetailLine.Id, XYZ.BasisY);
-                var newLine = Document.GetElement(newID.First()) as ModelLine;
-                var locCurve = newLine.Location as LocationCurve;
-                locCurve.Curve = Line.CreateBound(transform.OfPoint(line.GetEndPoint(0)), transform.OfPoint(line.GetEndPoint(1)));
-            }
-
-            //Delete old detail lines
-            Document.Delete(detailLines.Select(x => x.Id).ToList());
-
-            trans.Commit();
-        }
-
     }
 }
--- Commands/test.cs
+++ Commands/test.cs
@@ -1,58 +1,151 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using BimIshou.Utils;
 using Nice3point.Revit.Toolkit.External;
 using System.Windows;
 
-namespace BimIshou.Commands
+namespace RevitAddin
 {
     [Transaction(TransactionMode.Manual)]
-    public class test : ExternalCommand
+    public class CommandMoveEnd : ExternalCommand
     {
+        RevitCommandEndedMonitor revitCommandEndedMonitor;
+        IList<Reference> selectRooms;
+        public List<ElementId> AddedElement { get; set; } = new List<ElementId>();
+        List<ElementId> Ceillings = new();
         public override void Execute()
         {
-            List<XYZ> points = new List<XYZ>();
-            List<TextNote> texts = new List<TextNote>();
-            List<Data> datas = new List<Data>();
-
-            List<ElementId> elementsIds = UiDocument.Selection.GetElementIds()?.ToList() ?? new List<ElementId>();
-            List<Element> selectedElements = elementsIds.Count > 0 ? elementsIds.Select(x => Document.GetElement(x)).ToList() : new List<Element>();
-
-            foreach (Element _line in selectedElements)
+            try
             {
-                var line = _line as ModelLine;
-                if (line == null) continue;
-                XYZ p1 = line.GeometryCurve.GetEndPoint(0);
-                XYZ p2 = line.GeometryCurve.GetEndPoint(1);
-                if (!points.Exists(x => x.IsAlmostEqualTo(p1))) points.Add(p1);
-                if (!points.Exists(x => x.IsAlmostEqualTo(p2))) points.Add(p2);
+                selectRooms = UiDocument.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element, new SelectionFilter(BuiltInCategory.OST_Rooms, true));
+                revitCommandEndedMonitor = new RevitCommandEndedMonitor(UiApplication);
+                revitCommandEndedMonitor.CommandEnded += OnCommandEnded;
+                Application.DocumentChanged += Application_DocumentChanged;
+                UiApplication.PostCommand(RevitCommandId.LookupPostableCommandId(PostableCommand.AutomaticCeiling));
             }
-            foreach (Element _text in selectedElements)
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
             {
-                var text = _text as TextNote;
-                if (text == null) continue;
-                texts.Add(text);
+                return;
             }
-            foreach (TextNote text in texts)
+        }
+
+        private void Application_DocumentChanged(object sender, Autodesk.Revit.DB.Events.DocumentChangedEventArgs e)
+        {
+            AddedElement.AddRange(e.GetAddedElementIds());
+        }
+
+        private void OnCommandEnded(object sender, EventArgs e)
+        {
+            Application.DocumentChanged -= Application_DocumentChanged;
+            revitCommandEndedMonitor.CommandEnded -= OnCommandEnded;
+            foreach (var item in AddedElement)
             {
-                var value = text.Text.Split('+');
-                var loca = text.GetLeaders().First().End;
-                foreach (var p in points)
+                var ele = Document.GetElement(item);
+                if (ele is Ceiling)
                 {
-                    if (loca.DistanceTo(p) < 10)
-                    {
-                        datas.Add(new Data(text.Id.IntegerValue, loca, value[0], value[1]));
-                    }
+                    Ceillings.Add(ele.Id);
+                    break;
                 }
             }
-            datas.OrderByDescending(x => x.id);
-            MessageBox.Show(datas[0].s1);
+            using (TransactionGroup tranG = new TransactionGroup(Document, "Auto Create Ceilling"))
+            {
+                tranG.Start();
+                using (Transaction transs = new Transaction(Document, "test"))
+                {
+                    transs.Start();
+                    var opt = new SpatialElementBoundaryOptions();
+                    for (int i = 1; i < selectRooms.Count; i++)
+                    {
+                        var tempCeilling = (Document.GetElement(Ceillings.FirstOrDefault())).Copy(XYZ.BasisY * 100 * i).FirstOrDefault();
+                        Ceillings.Add(tempCeilling);
+                    }
+                    transs.Commit();
+                }
+                for (int i = 0; i < selectRooms.Count; i++)
+                {
+                    ICollection<ElementId> ids;
+                    List<Line> polygon = new List<Line>();
+                    Room room = Document.GetElement(selectRooms[i]) as Room;
+                    IList<IList<BoundarySegment>> loops = room.GetBoundarySegments(new SpatialElementBoundaryOptions());
+                    foreach (IList<BoundarySegment> loop in loops)
+                    {
+                        foreach (BoundarySegment seg in loop)
+                        {
+                            Line line = seg.GetCurve() as Line;
+                            polygon.Add(line);
+                        }
+                    }
+                    using (Transaction transs = new Transaction(Document, "temp"))
+                    {
+                        transs.Start();
+                        ids = Document.Delete(Ceillings[i]);
+                        transs.RollBack();
+                    }
+                    using (Transaction trans = new Transaction(Document, "Change Sketch Ceilling"))
+                    {
+                        trans.Start();
+                        EditCeilling(ids, polygon);
+                        trans.Commit();
+                    }
+                }
+                tranG.Assimilate();
+            }
         }
-        public record Data(int id, XYZ loca, string s1, string s2)
+        private void EditCeilling(ICollection<ElementId> elementIds, List<Line> polygon)
         {
-            public int id { get; set; } = id;
-            public XYZ loca { get; set; } = loca;
-            public string s1 { get; set; } = s1;
-            public string s2 { get; set; } = s2;
+            List<ModelLine> detailLines = new List<ModelLine>();
+            foreach (ElementId id in elementIds)
+            {
+                Element ele = Document.GetElement(id);
+                if (ele is ModelLine)
+                {
+                    detailLines.Add(ele as ModelLine);
+                }
+            }
+            var firstDetailLine = detailLines.FirstOrDefault();
+            foreach (var line in polygon)
+            {
+                Transform transform = Transform.CreateTranslation(new XYZ(0, 0, 1));
+                var tf = Transform.Identity;
+                var newID = ElementTransformUtils.CopyElement(Document, firstDetailLine.Id, XYZ.BasisY);
+                var newLine = Document.GetElement(newID.First()) as ModelLine;
+                var locCurve = newLine.Location as LocationCurve;
+                locCurve.Curve = Line.CreateBound(transform.OfPoint(line.GetEndPoint(0)), transform.OfPoint(line.GetEndPoint(1)));
+            }
+            Document.Delete(detailLines.Select(x => x.Id).ToList());
+        }
+    }
+
+    public class RevitCommandEndedMonitor
+    {
+        private readonly UIApplication _revitUiApplication;
+
+        private bool _initializingCommandMonitor;
+
+        public event EventHandler CommandEnded;
+
+        public RevitCommandEndedMonitor(UIApplication revituiApplication)
+        {
+            _revitUiApplication = revituiApplication;
+            _initializingCommandMonitor = true;
+            _revitUiApplication.Idling += OnRevitUiApplicationIdling;
+        }
+        private void OnRevitUiApplicationIdling(object sender, IdlingEventArgs idlingEventArgs)
+        {
+            if (_initializingCommandMonitor)
+            {
+                _initializingCommandMonitor = false;
+                return;
+            }
+            _revitUiApplication.Idling -= OnRevitUiApplicationIdling;
+            OnCommandEnded();
+        }
+        protected virtual void OnCommandEnded()
+        {
+            CommandEnded?.Invoke(this, EventArgs.Empty);
         }
     }
 }
