5084eb5053fb02093c3eebc3a8b9b4701cd44b6d
My PC <My PC@DESKTOP-DG3329I>
2023-12-25 22:43:42 +0700

    update

--- Commands/Export2Excel.cs
+++ Commands/Export2Excel.cs
@@ -1,8 +1,9 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Microsoft.Office.Interop.Excel;
+using Nice3point.Revit.Extensions;
 using Nice3point.Revit.Toolkit.External;
-using NPOI.SS.UserModel;
+using System.Diagnostics;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace BimIshou.Commands
@@ -12,8 +13,11 @@ namespace BimIshou.Commands
     {
         public override void Execute()
         {
-            string path = @"C:\Users\HC-07\Desktop\";
-            ViewSchedule viewSchedule = Document.GetElement(new ElementId(3348)) as ViewSchedule;
+            var allSheet = GetAllSheet();
+            var allRevision = Revision.GetAllRevisionIds(Document);
+
+            string path = @"C:\Users\My PC\Desktop";
+            ViewSchedule viewSchedule = Document.GetElement(new ElementId(7920)) as ViewSchedule;
 
             ViewScheduleExportOptions exportOptions = new ViewScheduleExportOptions();
             exportOptions.TextQualifier = ExportTextQualifier.DoubleQuote;
@@ -22,36 +26,65 @@ namespace BimIshou.Commands
             exportOptions.HeadersFootersBlanks = false;
 
             viewSchedule.Export(path, "test.csv", exportOptions);
-
             Excel.Application excelApp = new Excel.Application();
 
             // Mở một workbook từ file
-            Excel.Workbook workbook = excelApp.Workbooks.Open(path + "test.csv");
+            Workbook workbook = excelApp.Workbooks.Open(path + "\\test.csv");
+            try
+            {
+                // Lấy ra worksheet đầu tiên (hoặc theo tên)
+                Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+                var rng = worksheet.UsedRange;
+                int colCount = rng.Columns.Count;
+                int rowCount = rng.Rows.Count;
 
-            // Lấy ra worksheet đầu tiên (hoặc theo tên)
-            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets[1];
+                rng = (Range)worksheet.Cells[rowCount, colCount];
 
-            // Tạo dòng mới tại dòng tiếp theo
-            Excel.Range newDataRow = (Range)worksheet.Rows[0];
+                for (int i = 0; i < allRevision.Count; i++)
+                {
+                    Revision revision = Document.GetElement(allRevision[i]) as Revision;
+                    worksheet.Cells[1, colCount + 1 + i] = revision.Description;
+                }
 
-            // Đặt giá trị cho các ô trong dòng mới
-            //newDataRow.Cells[1, 1] = "New Value 1";
-            //newDataRow.Cells[1, 2] = "New Value 2";
-            // ... Đặt giá trị cho các ô khác tùy thuộc vào nhu cầu của bạn
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    string sheetNumber = (worksheet.Cells[i, 2] as Range).Value.ToString();
+                    var sheet = allSheet.Where(sheet => sheet.SheetNumber.Equals(sheetNumber)).FirstOrDefault();
+                    var allrevisionofSheet = sheet.GetAllRevisionIds();
+                    foreach (var item in allrevisionofSheet)
+                    {
+                        Revision revision = Document.GetElement(item) as Revision;
+                        for (int j = colCount + 1; j < colCount + 1 + allRevision.Count; j++)
+                        {
+                            string value = (worksheet.Cells[1, j] as Range).Value.ToString();
+                            if (revision.Description.Equals(value))
+                            {
+                                worksheet.Cells[i, j] = revision.RevisionDate;
+                            }
+                        }
+                    }
+                }
 
-            // Lưu workbook
-            workbook.Save();
-
-            // Đóng workbook và ứng dụng Excel
-            workbook.Close();
-            excelApp.Quit();
-
-            //ViewSheet viewSheet = Document.GetElement(new ElementId(3041)) as ViewSheet;
-            //var xxxx = viewSheet.GetAllRevisionIds();
-            //foreach ( var x in xxxx )
-            //{
-            //    var revision = x.ToElement(Document) as Revision;
-            //}
+                // Lưu workbook
+                workbook.Save();
+                // Đóng workbook và ứng dụng Excel
+                workbook.Close();
+                excelApp.Quit();
+            }
+            catch (Exception)
+            {
+                workbook.Close();
+                excelApp.Quit();
+                throw;
+            }
+        }
+        private List<ViewSheet> GetAllSheet()
+        {
+            return new FilteredElementCollector(Document)
+                .OfClass(typeof(ViewSheet))
+                .WhereElementIsNotElementType()
+                .Cast<ViewSheet>()
+                .ToList();
         }
     }
 }
